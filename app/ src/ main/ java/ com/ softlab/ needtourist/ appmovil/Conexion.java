705455c17d73e60276f1f0dde72aafe5dfc0691a
cherubinyambay <cherubinyambay@gmail.com>
2019-01-18 08:07:13 -0500

    Update and rename app/src/main/java/com/softlab/needtourist/appmovil/Conexion.java to  app/ src/ main/ java/ com/ softlab/ needtourist/ appmovil/Conexion.java

---
+++ app/ src/ main/ java/ com/ softlab/ needtourist/ appmovil/Conexion.java
@@ -0,0 +1,6 @@
+package com.softlab.needtourist.appmovil;
+
+
+public class Conexion {
+    public static final String URL_WEB_SERVIhttps://vitaeplus.000webhostapp.com/appVitaePLU
+}
--- app/src/main/java/com/softlab/needtourist/appmovil/Conexion.java
+++
@@ -1,9 +0,0 @@
-package com.softlab.needtourist.appmovil;
-
-/**
- * Created by meman94 on 22/11/2018.
- */
-
-public class Conexion {
-    public static final String URL_WEB_SERVICE = "https://vitaeplus.000webhostapp.com/appVitaePLUS/";
-}
